2dfd3cb93c91036e949b8276ef148779fffe8da6
Phil Lavin <phil.lavin@synety.com>
2018-05-18 17:07:04 +0100

    presence: fix crush if dialog id is NULL
    - check dialog_id is not NULL before call delete_presentity_if_dialog_id_exists
    
    (cherry picked from commit 41806a1)

--- modules/presence/presentity.c
+++ modules/presence/presentity.c
@@ -691,9 +691,11 @@ int update_presentity(struct sip_msg* msg, presentity_t* presentity, str* body,
 
 			check_if_dialog(*body, &is_dialog, &dialog_id);
 
-			if (delete_presentity_if_dialog_id_exists(presentity, dialog_id) < 0) {
-				free(dialog_id);
-				goto error;
+			if (dialog_id) {
+				if (delete_presentity_if_dialog_id_exists(presentity, dialog_id) < 0) {
+					free(dialog_id);
+					goto error;
+				}
 			}
 
 			free(dialog_id);
